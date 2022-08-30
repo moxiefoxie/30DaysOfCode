d9adf400df5dfba15f80d7660e7d50f98970f8b5
moxiefoxie <34554109+moxiefoxie@users.noreply.github.com>
2022-08-30 14:55:14 -0500

    Update Day4.cs

--- Day4.cs
+++ Day4.cs
@@ -30,3 +30,16 @@ class Person {
      }
 
 static void Main(String[] args) {
+        int T=int.Parse(Console.In.ReadLine());
+        for (int i = 0; i < T; i++) {
+            int age=int.Parse(Console.In.ReadLine());
+            Person p=new Person(age);
+             p.amIOld();
+                for (int j = 0; j < 3; j++) {
+                  p.yearPasses();
+                }
+                p.amIOld();
+                Console.WriteLine();
+        }
+    }
+}
