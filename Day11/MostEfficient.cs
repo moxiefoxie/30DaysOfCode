fa583c07dbfc8899b6cd3db0c2b035ef073fbe43
moxiefoxie <34554109+moxiefoxie@users.noreply.github.com>
2022-09-07 15:40:14 -0500

    Update MostEfficient.cs

--- Day11/MostEfficient.cs
+++ Day11/MostEfficient.cs
@@ -17,8 +17,9 @@ using System;
 class Solution
 {
     //extract the hourglasses
-    public static int getMaxHourglassSum(List<List<int>> pattern)
+    public static int? getMaxHourglassSum(List<List<int>> pattern)
     {
+        //initiate values
         int width = 3;
         int matrixwidth = pattern[0].Count;
         int matrixheight = pattern.Count;
@@ -26,12 +27,21 @@ class Solution
         List<int> hourglass = new List<int>();
         List<List<int>> hourglasses = new List<List<int>>();
         
+        //check that the matrix is square
+        if(matrixwidth != matrixheight)
+        {
+            return null;
+        }
+        
+        //loop through matrix
         for(int i = 0; i < (matrixwidth-(width-1)) * (matrixheight-(width-1)) ; i++)
         {
          
             // Find row and column index
             int row = i / (matrixheight-(width-1)) ;
             int col = i % (matrixwidth-(width-1)) ;
+            
+            //make a new hourglass
             hourglass = new List<int>();
             
             //extract top
@@ -41,7 +51,10 @@ class Solution
             //extract bottom
             hourglass.AddRange(pattern[row+2].GetRange(col, width));
             
+            //find sum
             int sum = hourglass.Sum();
+            
+            //check if sum is the max
             if(sum > max)
             {
                 max = sum;
@@ -54,10 +67,14 @@ class Solution
 
         List<List<int>> arr = new List<List<int>>();
         //if this took in any number of rows, this would be O(n^3)
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < 7; i++)
         {
             arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
         }
-        Console.WriteLine(getMaxHourglassSum(arr));
+        var answer = getMaxHourglassSum(arr);
+        if(answer == null)
+            Console.WriteLine("Please submit a square matrix.");
+        else
+            Console.WriteLine(getMaxHourglassSum(arr));
     }
 }
