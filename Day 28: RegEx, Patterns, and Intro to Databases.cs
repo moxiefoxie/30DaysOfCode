00ccd9ea14ce7a79064214eee85787bd4291a635
moxiefoxie <34554109+moxiefoxie@users.noreply.github.com>
2022-10-31 15:45:14 -0500

    Create Day 28: RegEx, Patterns, and Intro to Databases.cs

---
+++ Day 28: RegEx, Patterns, and Intro to Databases.cs
@@ -0,0 +1,40 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
+        List<string> gmailAccountNames = new List<string>();
+        for (int NItr = 0; NItr < N; NItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+            
+            string firstName = firstMultipleInput[0];
+
+            string emailID = firstMultipleInput[1];
+            
+            if(Regex.IsMatch(emailID, "@gmail.com"))
+                gmailAccountNames.Add(firstName);
+        }
+        gmailAccountNames.Sort((x, y) => string.Compare(x, y));
+        foreach(var name in gmailAccountNames) {
+            Console.WriteLine(name);
+        }
+    }
+
+}
